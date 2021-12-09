c66445133bbaf8eb0f5a6dbff9cabd7a7f3c2206
Svetlana <Svetlana@DESKTOP-UVJFVQS>
2021-12-09 20:32:46 +0300

    Добавьте файлы проекта.

---
+++ Proverka/Proverka2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proverka
+{
+    public partial class Proverka2 : Form
+    {
+        public Proverka2()
+        {
+            InitializeComponent();
+        }
+    }
+}
